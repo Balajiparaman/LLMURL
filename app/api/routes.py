60136dcabbe06c3e31645ed9379ea86b03304586
Balaji Paraman <balajiparaman1998@gmail.com>
2024-03-09 14:30:17 +0530

    committing code changes

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,4 +1,10 @@
 from . import api_blueprint
+from flask import requests, jsonify
+from app.services import openai_service, pinecone_service, scraping_service
+from app.utils.helper_functions import chunk_text
+
+PINECONE_INDEX_NAME = 'index007'
+
 
 @api_blueprint.route("/embed-and-store", methods=["POST"])
 def embed_and_store():
@@ -8,7 +14,17 @@ def embed_and_store():
     2. Embedding the texts
     3. Uploading to the vector database
     '''
-    pass
+    url = requests.json["url"]
+    url_text = scraping_service.scrape_website(url)
+    chunks = chunk_text(url_text)
+    pinecone_service.embed_chunks_and_upload_to_pinecone(
+        chunks, PINECONE_INDEX_NAME)
+    response_json = {
+        "message": "Chunks embedded and uploaded successfully to Pinecone"
+    }
+
+    return jsonify(response_json)
+
 
 @api_blueprint.route("/handle-query", methods=["POST"])
 def handle_query():
--- app/services/pinecone_service.py
+++ app/services/pinecone_service.py
@@ -10,7 +10,14 @@ EMBEDDING_DIM = 256
 
 
 def embed_chunks_and_upload_to_pinecone(chunks, index_name):
+    '''
+    This function takes chunks(list of text chunks) and index_name(Pinecone index name) as inputs
+    and uploads the embeddings along with the related metadata(chunk, ID) to the Pinecone Vector DB
+    for efficient similarity search based on these embeddings.
+
+    '''
     if index_name in pinecone.list_indexes():
+        # if index name already exists within Pinecone, delete it.
         pinecone.delete_index(name=index_name)
 
     pinecone.create_index(name=index_name,
@@ -27,3 +34,20 @@ def embed_chunks_and_upload_to_pinecone(chunks, index_name):
     upserts = [(id, vec, {"chunk_text": chunk})
                for id, vec, chunk in embeddings_with_ids]
     index.upsert(vectors=upserts)
+
+
+def get_most_similar_chunks_for_query():
+    '''
+    The function does the following actions:
+    1. Embeds the question using openai embedding model via the get_embedding func
+    2. Query the Pinecone DB to find the top 3 text chunks most similar to the question using cosine similarity algo
+    3. Returns the texts of those chunks as a list
+    '''
+    question_embedding = get_embedding(query)
+    index = pinecone.Index(index_name)
+    query_results = index.query(
+        question_embedding, top_k=3, include_metadata=True)
+    context_chunks = [x["metadata"]["chunk_text"]
+                      for x in query_results["matches"]]
+
+    return context_chunks
